7d6045763107ac812a01068684a26035665099ff
Bochkarev Ivan <ivanx86@gmail.com>
2023-04-21 13:07:19 +0600

    feat(components) Доработана шапка

--- src/blocks/sections/s_header/s_header.js
+++ src/blocks/sections/s_header/s_header.js
@@ -2,19 +2,29 @@ import { Swiper, Pagination, Navigation, Lazy } from "swiper";
 
 Swiper.use([Pagination, Navigation, Lazy]);
 
-const breakpointScroll = 625;
 let header = document.querySelector(".s-header"),
   headerBurger = document.querySelector(".s-header__burger"),
-  backdrop = document.querySelector(".backdrop");
+  headerNavigation = document.querySelector(".s-header__nav"),
+  backdrop = document.querySelector(".backdrop"),
+  breakpointScroll =
+    document.getElementsByClassName("s-header")["0"].offsetHeight,
+  bodyBlock = document.querySelector("body");
 
 document.addEventListener("DOMContentLoaded", function () {
   // Открытие/Закрытие мега меню по клику на бургер
   headerBurger.addEventListener("click", () => {
     headerBurger.classList.toggle("is-open");
-    if (backdrop) {
-      backdrop.classList.toggle("is-active");
-    }
-    document.body.classList.toggle("is-blocked");
+    headerNavigation.classList.toggle("is-show");
+    backdrop.classList.toggle("is-active");
+    bodyBlock.classList.toggle("is-blocked");
+  });
+
+  // Закрытие меню по клику вне меню
+  backdrop.addEventListener("click", () => {
+    headerBurger.classList.remove("is-open");
+    backdrop.classList.remove("is-active");
+    bodyBlock.classList.remove("is-blocked");
+    headerNavigation.classList.remove("is-show");
   });
 
   // Фиксация шапки при скроле
